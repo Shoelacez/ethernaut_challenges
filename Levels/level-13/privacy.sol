f4bca3868d0f61321a690b20068196714d7a5d13
1300Hrs <92827559+Shoelacez@users.noreply.github.com>
2023-03-20 07:19:07 +0800

    Privacy Level 12

--- Levels/level-13/privacy.sol
+++ Levels/level-13/privacy.sol
@@ -6,11 +6,11 @@ contract Privacy {
     bool public locked = true;
     // slot 1
     uint256 public ID = block.timestamp;
-    // slot 2 (takes 2B)
+    // slot 2 (takes 2Bytes)
     uint8 private flattening = 10;
-    // slot 2 (takes 2B)
+    // slot 2 (takes 2Bytes)
     uint8 private denomination = 255;
-    // slot 2 (takes 2B)
+    // slot 2 (takes 2Bytes)
     uint16 private awkwardness = uint16(block.timestamp);
     // slot 3(element at 0),4(element at 1) and 5(element at 2)
     bytes32[3] private data;
@@ -18,19 +18,9 @@ contract Privacy {
     constructor(bytes32[3] memory _data) {
         data = _data;
     }
-    // key: 0x5d98b8d651220ed41f00cd5bf0e3a079
+    // key from storage: 0x5d98b8d651220ed41f00cd5bf0e3a079
     function unlock(bytes16 _key) public {
         require(_key == bytes16(data[2]));
         locked = false;
     }
-
-    /*
-        A bunch of super advanced solidity algorithms...
-
-        ,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`
-        .,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,
-        *.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^         ,---/V\
-        `*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.    ~|__(o.o)
-        ^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'  UU  UU
-    */
 }
